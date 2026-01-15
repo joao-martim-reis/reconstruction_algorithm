80f402756212d94bcb6a2a11ad3783cad29f5e3e
Joao Reis <joaomartimreis@ua.pt>
2026-01-15 13:16:02 +0000

    test

--- HU_conversion.py
+++ HU_conversion.py
@@ -7,6 +7,8 @@ def HU_conversion(volume, water_val, air_val):
     Automatically generates the calibration line graph.
 
     """
+
+    "so para ver se esta a dar"
     
     # 1. Define the reference physical values
     HU_water = 0.0
